bc86951937905a3ad6832c66446f4732e51affc0
mari0mejia <mariomejia4263@yahoo.com>
2020-08-04 16:33:02 -0700

    Fix robot arm display

--- tech_career_fair/settings.py
+++ tech_career_fair/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+try:
+    from .local_settings import *
+except ImportError:
+    print("No local file. You must be on production.")
